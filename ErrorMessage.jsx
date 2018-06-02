1ebab3fb4772613bcac4ba9c06702f6284486669
Walker Angell <w@lker.co>
2018-06-02 15:44:06 -0400

    add fieldName to ErrorMessage

--- ErrorMessage.jsx
+++ ErrorMessage.jsx
@@ -3,8 +3,9 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
 
-const ErrorMessage = ({ message, code }) => (
+const ErrorMessage = ({ message, code, fieldName }) => (
   <li className={classNames('error', { [`code_${code}`]: !!code })}>
+    { fieldName && <span className='field-name'>{ fieldName }: </span> }
     { message }
   </li>
 );
@@ -12,10 +13,12 @@ const ErrorMessage = ({ message, code }) => (
 ErrorMessage.propTypes = {
   message: PropTypes.node.isRequired,
   code: PropTypes.string,
+  fieldName: PropTypes.string,
 };
 
 ErrorMessage.defaultProps = {
   code: null,
+  fieldName: null,
 };
 
 export default ErrorMessage;
